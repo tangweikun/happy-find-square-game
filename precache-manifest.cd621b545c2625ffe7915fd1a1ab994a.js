18fdb4d6067c0426840420f7bf4ee66777d83af3
tangweikun <819573105@qq.com>
2019-11-21 11:39:24 +0800

    Updates

---
+++ precache-manifest.cd621b545c2625ffe7915fd1a1ab994a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b47ede124f955e4251aeac697628a985",
+    "url": "/happy-find-square-game/index.html"
+  },
+  {
+    "revision": "e26e0bef215bc61a293e",
+    "url": "/happy-find-square-game/static/css/main.1eb886ff.chunk.css"
+  },
+  {
+    "revision": "bb8922738f0e939fa5be",
+    "url": "/happy-find-square-game/static/js/2.09a38b1b.chunk.js"
+  },
+  {
+    "revision": "e26e0bef215bc61a293e",
+    "url": "/happy-find-square-game/static/js/main.2829814d.chunk.js"
+  },
+  {
+    "revision": "0514ce6a94a3a83806b4",
+    "url": "/happy-find-square-game/static/js/runtime-main.f8f9ecfb.js"
+  }
+]);
